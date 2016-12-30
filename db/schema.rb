3b30dd64efe94facd20cdf477ae7c227eb85b187
Strawberry <strawberrycsgo@gmail.com>
2016-12-30 08:46:55 +0700

    Modeling users

---
+++ db/schema.rb
@@ -0,0 +1,9 @@
+ActiveRecord::Schema.define version: 20161229095209 do
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+  end
+end
